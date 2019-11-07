55d40b1b4696b70fe63e9d3eaa1238de484039cb
yannickalr <56535516+yannickalr@users.noreply.github.com>
2019-11-07 14:53:55 +0100

    Update formulaire.js

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,33 +1,63 @@
-// Il y a plusieurs façon de sélectionner un nœud DOM ; ici on récupère
-// le formulaire et le champ d'e-mail ainsi que l'élément span
-// dans lequel on placera le message d'erreur
+function validation() {
+var nomValue = document.querySelector("#name").value;
+var prenomValue = document.querySelector("#surname").value;
+var dateValue = document.querySelector("#birthday").value;
+var mailValue = document.querySelector("#mail").value;
+var addressValue = document.querySelector("#address").value;
 
 
+      if (nomValue.length==0){
+                   document.querySelector("#error").textContent="le champs nom est vide";
+                   document.querySelector("#result").textContent="";
+                }
+      else
 
-  var formValid = document.getElementById('bouton_envoi');
-  var nom = document.getElementById('nom');
-  var missNom = document.getElementById('missNom');
+      if (nomValue.length<5){
+                   document.querySelector("#error").textContent="le nom doit contenir au moins 5 caracteres";
+                   document.querySelector("#result").textContent="";
+                }
 
-  formValid.addEventListener('click', validation );
-  function validation(event)
+      else if  (prenomValue.length==0){
+                   document.querySelector("#error").textContent="le champs prenom est vide";
+                   document.querySelector("#result").textContent="";
+                }
+      else if (prenomValue.length<5){
+                   document.querySelector("#error").textContent="le prenom doit contenir au moins 5 caracteres";
+                   document.querySelector("#result").textContent="";
+                }
 
+     else if(dateValue.length==0){
+                                 document.querySelector("#error").textContent="le champs date est vide";
+                                 document.querySelector("#result").textContent="";
+                              }
 
-function validation()
+     else if (mailValue.length==0){
+                   document.querySelector("#error").textContent="le champs mail est vide";
+                   document.querySelector("#result").textContent="";
+                }
+     else if (mailValue.length<5){
+                   document.querySelector("#error").textContent="le mail doit contenir au moins 5 caracteres";
+                   document.querySelector("#result").textContent="";
+                }
 
-{
-  //pour un champs vide
-  if (nom.validity.valueMissing){
-      event.preventDefault();
-      missNom.textContent = 'Le nom doit contenir au moins 5 caractere';
-      missNom.style.color = 'red';
-  //si le format est incorrect
-}  else if (nom.validity.test(prenom.value) == false ){
-      event.preventDefault();
-      missNom.textContent = 'format pas correct';
-      missNom.style.color = 'green';
+     else if (addressValue.length==0){
+                   document.querySelector("#error").textContent="le champs address est vide";
+                   document.querySelector("#result").textContent="";
+                }
+    else if (addressValue.length<5){
+                   document.querySelector("#error").textContent="l'addresse doit contenir au moins 5 caracteres";
+                   document.querySelector("#result").textContent="";
+                }
+                else
 
-  }
-  else{
+                if (nomValue.length>=5
+                  && prenomValue.length>=5
+                  && dateValue.length !==0
+                  && mailValue.length>=5
+                  && addressValue.length>=5)
+                  {
+                   document.querySelector("#result").textContent="Bienvenue" + document.querySelector(    "#name").value + document.querySelector("#surname").value;
+                   document.querySelector("#error").textContent="";
+                }
 
-  }
 }
